55a87ab1a19a6906324842020e0c78911d127219
Jordan Schmelzer <jordanschmelzer@gmail.com>
2024-07-21 22:52:19 -0500

    Chess App Initial Commit

---
+++ web-chess/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { GameComponent } from "./game/game.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, GameComponent],
+  templateUrl: './app.component.html',
+  styles: `
+
+  `
+})
+export class AppComponent {
+  title = 'web-chess';
+}
