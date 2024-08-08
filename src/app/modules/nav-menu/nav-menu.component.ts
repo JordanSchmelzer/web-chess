5a41cdacc8ec3c80ed3d23a2fe2f79ff1f4e7230
Jordan Schmelzer <jordanschmelzer@gmail.com>
2024-08-07 22:00:47 -0500

    todo: routing for friend play or computer play boards

--- src/app/modules/nav-menu/nav-menu.component.ts
+++ src/app/modules/nav-menu/nav-menu.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { RouterModule } from '@angular/router';
+import { BoardComponent } from "../board/board.component";
 
 @Component({
   selector: 'app-nav-menu',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [MatToolbarModule, MatButtonModule, RouterModule, BoardComponent],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.css'
 })
